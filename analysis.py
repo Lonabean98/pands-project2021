316a48420ffee62155e9800e4d608a0fb769087d
Lonan Keane <LonanKeane>
2021-03-28 17:44:30 +0100

    Further updates to analysis.py

--- analysis.py
+++ analysis.py
@@ -1,18 +1,14 @@
-#with open('iris.data', 'r') as f:
-    #data=f.read()
-    #print(data)
-
 import pandas as pd 
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+import random
 
 df = pd.read_csv("iris.data")  
 df.columns = ["sepal length", "sepal width", "petal length", "petal width", "variety"]
 
-#head() command shows first x number of lines. Specify number in bracket  
-print(df.head(10))  
-
+#prints data from random sample of 10 flowers
+print(df.sample(10))  
 
 summary = df.describe(include="all")  
 summaryt = summary.transpose()  
@@ -20,5 +16,50 @@ print("")
 print("Summary Statistics")
 print(summaryt)
 
-#plt.hist(df.columns[1])
-#plt.show()
+
+#This function takes in a column name and colour and uses 
+# this to plot the histogram
+def plothist(x, y):
+    plt.hist(df[x], color =y)
+    plt.title(x)
+    plt.xlabel("lenght in cm")
+    plt.ylabel("Frequency")
+    #plt.savefig(x +".png")
+    #plt.clf()
+    #plt.show()
+
+
+#for columnname in df.columns:
+   # colours=["Red", "Yellow", "Blue", "Green"]
+    #if columnname != "variety":
+        #for colour in colours:
+            #plothist(columnname, colour)
+      
+#plothist('sepal length', "red")
+#plothist('sepal width', "yellow")
+#plothist("petal length", "blue")
+#plothist("petal width", "green")
+
+#def plotscatter(x,y):
+   # plt.scatter(df[x,y])
+    #plt.show()
+
+def scatterSepal():
+    x=df["sepal length"]
+    y=df["sepal width"]
+    plt.xlabel("Sepal Length (cm)")
+    plt.ylabel("Sepal Width (cm)")
+    plt.title("Sepal Width vs Sepal Lenght")
+    plt.scatter(x,y)
+    plt.show()
+scatterSepal()
+
+def scatterPetal():
+    x=df["petal length"]
+    y=df["petal width"]
+    plt.xlabel("Petal Length (cm)")
+    plt.ylabel("Petal Width (cm)")
+    plt.title("Petal Width vs Petal Lenght")
+    plt.scatter(x,y, color= "red")
+    plt.show()
+scatterPetal()
