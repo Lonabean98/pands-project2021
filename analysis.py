e04a5bbe4d0b802b830c6bd7783310198229a477
Lonan Keane <LonanKeane>
2021-04-05 22:32:07 +0100

    added correct scatter function

--- analysis.py
+++ analysis.py
@@ -2,69 +2,80 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import random
+import seaborn as sns
 
-# reads a comma-separated values (csv) file into DataFrame as 'df'
-#Reference: https://pandas.pydata.org/docs/reference/api/pandas.read_csv.html
+
 df = pd.read_csv("iris.data")  
+# reads a comma-separated values (csv) file into DataFrame as 'df'
+#Reference: https://pandas.pydata.org/docs/reference/api/pandas.read_csv.htm
 
-df.columns = ["sepal length", "sepal width", "petal length", "petal width", "variety"]
+df.columns = ["Sepal Length (cm)", "Sepal Width (cm)", "Petal Length (cm)", "Petal Width (cm)", "Variety"]
 
-#prints data from random sample of 10 flowers
+
 print(df.sample(10))  
+#prints data from random sample of 10 flowers
 
-#function that reads the data and prints a sumamry of the variables.
+
 def readData():
+    #function that reads the data and prints a sumamry of the variables.
+
+    
+    summary = df.describe()
     #df.describe() generates descriptive statistics. 
     #Reference: https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.describe.html
-    summary = df.describe()
+    summary2 = summary.transpose() 
     #transpose index and columns
-    #  Reference: https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.transpose.html
-    summary2 = summary.transpose()  
+    #  Reference: https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.transpose.html 
     print("")
     print("Summary Statistics")
     print(summary2)
-    # write the transposed summary into a .txt file called 'Summary'.
     with open('Summary.txt', 'w') as f:
         f.write(str(summary2))
+    # write the transposed summary into a .txt file called 'Summary'.
 readData()
 
 
 
-#This function takes in a column name and colour and uses 
-# this to plot the histogram
+
 def plothist(x, y):
+    #This function takes in a column name and colour and uses 
+    # this to plot the histogram
     plt.hist(df[x], color =y, ec="black")
+    #plot histogram using x and y variables
     plt.title(x)
-    plt.xlabel("lenght in cm")
+    substring= "Width"
+    if substring in x:
+        plt.xlabel("Width in cm")
+    else:
+        plt.xlabel("Length in cm")
+    #If the x value contains the string "Width", it will name the x axis "Width in cm"
+    #if not, it will name the x axis "Lenght in cm"
     plt.ylabel("Frequency")
     plt.savefig(x +".png")
     plt.clf()
     #plt.show()
 
 # plotting the histograms for each variable with different colours.
-plothist("sepal length", "red")
-plothist("sepal width", "blue")
-plothist("petal length", "green")
-plothist("petal width", "yellow")
+plothist("Sepal Length (cm)", "red")
+plothist("Sepal Width (cm)", "blue")
+plothist("Petal Length (cm)", "green")
+plothist("Petal Width (cm)", "yellow")
       
-# function that generates a scatter plot for both Sepal variables.
-def scatterSepal():
-    x=df["sepal length"]
-    y=df["sepal width"]
-    plt.xlabel("Sepal Length (cm)")
-    plt.ylabel("Sepal Width (cm)")
-    plt.title("Sepal Width vs Sepal Length")
-    plt.scatter(x,y)
-    plt.show()
-scatterSepal()
 
-# function that generates a scatter plot for both Petal variables.
-def scatterPetal():
-    x=df["petal length"]
-    y=df["petal width"]
-    plt.xlabel("Petal Length (cm)")
-    plt.ylabel("Petal Width (cm)")
-    plt.title("Petal Width vs Petal Length")
-    plt.scatter(x,y, color= "red")
+
+def scatter(x,y, z):
+    #function that generates a scatter plot for both Sepal variables.
+    plt.xlabel(x)
+    plt.ylabel(y)
+    plt.title(x+ " vs "+ y)
+    plt.scatter(df[x],df[y], c=z)
     plt.show()
-scatterPetal()
+    
+    
+scatter("Sepal Length (cm)", "Sepal Width (cm)", 'r')
+scatter("Sepal Length (cm)", "Petal Length (cm)", 'b')
+scatter("Sepal Length (cm)", "Petal Width (cm)", 'g')
+scatter("Sepal Width (cm)", "Petal Length (cm)", 'y')
+scatter("Sepal Width (cm)","Petal Width (cm)", 'purple' )
+scatter("Petal Width (cm)", "Petal Length (cm)" , 'cyan')
+
